e9980f0ce1dc47a5fa90b897c275bb41add375b8
Sean Scott <sean@sscottdev.com>
2022-10-16 13:10:44 -0400

    made the code less but more efficent

--- script.js
+++ script.js
@@ -5,63 +5,49 @@ let inputVal = input.value;
 const spanish = document.getElementById('eu');
 const russian = document.getElementById('ru');
 const chinese = document.getElementById('zh');
+const langSelect = document.getElementById('languageSelect')
 
 
+function loadingAnimation() {
+    document.getElementById("output").innerText = "Loading.";
+    setTimeout(function () {
+        document.getElementById("output").innerText = "Loading..";
+    } , 700);
+    setTimeout(function () {
+        document.getElementById("output").innerText = "Loading...";
+    } , 1000);
 
-function spanishSelect() {
-    languageSelect.innerText = `Selected language: Spanish`;
-    console.log("Spanish");
-    const options = {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/json',
-            'X-RapidAPI-Key': '3984098d58mshe329bdcd36f6853p1d8defjsn16e9ebd94eb2',
-            'X-RapidAPI-Host': 'deep-translate1.p.rapidapi.com',
-
-        },
-        body: JSON.stringify({q: document.getElementById("input").value, "source":"en", "target":"es"})
-    };
-    fetch('https://deep-translate1.p.rapidapi.com/language/translate/v2', options)
-        .then(response => response.json())
-        .then(response => document.getElementById("output").innerText = response.data.translations.translatedText)
-        .catch(err => console.error(err));
 }
 
 
-function russianSelect() {
-    languageSelect.innerText = `Selected language: Russian `;
-    console.log("Russian");
-    const options = {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/json',
-            'X-RapidAPI-Key': '3984098d58mshe329bdcd36f6853p1d8defjsn16e9ebd94eb2',
-            'X-RapidAPI-Host': 'deep-translate1.p.rapidapi.com',
+const KEY = '3984098d58mshe329bdcd36f6853p1d8defjsn16e9ebd94eb2';
 
-        },
-        body: JSON.stringify({q: document.getElementById("input").value, "source":"en", "target":"ru"})
-    };
-    fetch('https://deep-translate1.p.rapidapi.com/language/translate/v2', options)
-        .then(response => response.json())
-        .then(response => document.getElementById("output").innerText = response.data.translations.translatedText)
-        .catch(err => console.error(err));
+const HEADER_OPTIONS = {
+    'content-type': 'application/json',
+    'X-RapidAPI-Key': KEY,
+    'X-RapidAPI-Host': 'deep-translate1.p.rapidapi.com',
+
 }
 
-function frenchSelect() {
-    languageSelect.innerText = `Selected language: French `;
-    console.log("French");
+function langTranslate(lang){
+    let langToPrint = ''
+    switch (lang){
+        case "es": langToPrint = "Spanish"; break;
+        case "ru": langToPrint = "Russian"; break;
+        case "fr": langToPrint = "French"; break;
+    }
+    langSelect.innerText = `Selected langauge ${langToPrint}`
+    loadingAnimation();
+
     const options = {
         method: 'POST',
-        headers: {
-            'content-type': 'application/json',
-            'X-RapidAPI-Key': '3984098d58mshe329bdcd36f6853p1d8defjsn16e9ebd94eb2',
-            'X-RapidAPI-Host': 'deep-translate1.p.rapidapi.com',
-
-        },
-        body: JSON.stringify({q: document.getElementById("input").value, "source":"en", "target":"fr"})
-    };
+        headers: HEADER_OPTIONS,
+        body: JSON.stringify({q: document.getElementById("input").value, "source":"en", "target":lang})
+    }
     fetch('https://deep-translate1.p.rapidapi.com/language/translate/v2', options)
         .then(response => response.json())
-        .then(response => document.getElementById("output").innerText = response.data.translations.translatedText)
+        .then(response => {
+            document.getElementById("output").innerText = response.data.translations.translatedText
+        })
         .catch(err => console.error(err));
 }
